214734afa6b3d4b323dbfcd0d1388cd3526abdd8
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-06-23 00:14:34 +0900

    Create CodingTest_10870.java

---
+++ CodingTest_10870.java
@@ -0,0 +1,25 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class Test10870 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		
+		int arr [] = new int[N+2];
+		arr[0] = 0;
+		arr[1] = 1;
+
+		
+		for(int i = 2; i <= N; i++) {
+		 arr[i] = arr[i-2] + arr[i-1];
+			
+		}
+		
+		System.out.println(arr[N]);
+	}
+}
