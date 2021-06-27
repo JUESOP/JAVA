9a3f8d30284a3177ae8eb5b62896be021ad16c50
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-06-28 03:21:46 +0900

    Create Test_5622.java

---
+++ Test_5622.java
@@ -0,0 +1,33 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class T1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String x = sc.nextLine();       			 // 문자열로 입력 받음
+		char[] b = x.toCharArray();		 		  	 // 문자열을 char로 변환, b라는 배열 생성
+		
+		int answer = 0;
+		
+		int c[] = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10}; //c배열을 생성하여 차례대로 숫자를 넣어줌
+		
+		for(int i = 0; i < x.length(); i++) {
+			
+			int d = 0;									
+			d = b[i] - 65;  				 // d는 입력받은 알파벳 문자를 정수로 바꿔준 것 (65를 빼서 char -> int로 바꿔줌)
+			 
+			 answer += c[d];				 // d를 c의 인덱스 자리에 넣어줌 , answer에 저장
+			 
+			 continue;					  	 // 마지막 값만 출력되게 continue써줌
+			 
+				}
+		
+			System.out.println(answer);
+			
+		}
+}
+		
+
