08003b447757b45edd53ceee9436e520a0b6d925
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-06-28 12:23:43 +0900

    Create Test_음양더하기.java

---
+++ Test_음양더하기.java
@@ -0,0 +1,30 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class T2 {
+	
+//Ctrl + Shift + f 정렬
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arr[] = { 4, 7, 12 };
+		Boolean signs[] = { true, false, true };
+		int sum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (signs[i]) {
+
+				sum += arr[i];
+
+			} else {
+
+				sum -= arr[i];
+			}
+			
+		} //for
+		
+		System.out.println(sum);
+	}
+
+}
