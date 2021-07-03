7422ffa3e480807059efa2282b9a364917128498
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-07-04 00:02:50 +0900

    Update 정처기c++문제.cpp

--- 정처기c++문제.cpp
+++ 정처기c++문제.cpp
@@ -6,3 +6,16 @@ void main(){
 int i;
 int m1 = 0, m2 = 0;
 int a[5] = {4,2,3,5,1}
+
+for(i=0; i<5; i++){
+if(a[m1] > a[i]){
+m1 = i;
+}
+
+if(a[m2] < a[i]){
+m2 = i;
+ }
+}
+
+count << a[m1] << " ";
+count << a[m2];
