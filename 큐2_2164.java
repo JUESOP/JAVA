71eddf6039bcb9907a91cc548c55b824d65df526
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-07-07 12:41:52 +0900

    Update and rename Test_2164.java to 큐2_2164.java

--- 큐2_2164.java
+++ 큐2_2164.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Test2164 {
+public class Test1 {
 
 	public static void main(String[] args) {
 
@@ -13,17 +13,20 @@ public class Test2164 {
 
 		// Queue<자료형> Queue명 = new LinkedList<자료형>(크기);
 		Queue<Integer> q = new LinkedList<Integer>();
-		for (int i = 1; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			q.offer(i);
 		}
 
-		int num1 = 0;
-		while (q.size() != 1) {
-			num1 = q.poll();
+		while (true) {
+			q.poll();
 			q.offer(q.poll());
-		}
-		System.out.println(num1);
 
+			if (q.size() == 1) {
+				break;
+			}
+		}
+		
+		System.out.println(q.poll());
 	}
 
 }
