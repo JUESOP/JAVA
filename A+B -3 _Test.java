608c64397f3e8112d71bc66151bdbf9b4005d155
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-07-08 21:31:49 +0900

    Create A+B -3 _Test.java

---
+++ A+B -3 _Test.java
@@ -0,0 +1,21 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class T {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		for (int i = 0; i < N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			System.out.println(a + b);
+		}
+
+	}
+
+}
