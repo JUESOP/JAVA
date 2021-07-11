ce8e15c83bfe3964d2553204d2ea9545172bd15e
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-07-12 02:06:15 +0900

    Create 배열 오름차순 정렬.java

---
+++ 배열 오름차순 정렬.java
@@ -0,0 +1,29 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class T3 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int arr[] = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+			System.out.println(arr[i]);
+		}
+	}
+
+}
