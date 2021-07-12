10e926228ea14a3939b14c56ffe56ee73dcc9bb0
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-07-12 15:41:43 +0900

    Create Test_2884알람시계.java

---
+++ Test_2884알람시계.java
@@ -0,0 +1,25 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+
+		M -= 45;
+
+		if (M < 0) {
+			M += 60;
+			H--;
+		}
+		if (H < 0) {
+			H += 24;
+		}
+		
+		System.out.print(H + " " + M);
+	}
+}
