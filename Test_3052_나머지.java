bb31f139dbe8269df286a88e972272d871b738a1
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-07-16 01:16:17 +0900

    Update Test_3052_나머지.java

--- Test_3052_나머지.java
+++ Test_3052_나머지.java
@@ -1,23 +1,25 @@
 package code;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class T_나머지 {
 
 	public static void main(String[] args) {
 
+		HashSet<Integer> hashset = new HashSet<>();
 		Scanner sc = new Scanner(System.in);
 		int arr[] = new int[10];
-		int num = 0;
 
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 
-			num += arr[i] % 42;
+			int num = arr[i] % 42;
+
+			hashset.add(num);
 		}
 
-		System.out.println(num);
-
+		System.out.println(hashset.size());
 	}
 
 }
