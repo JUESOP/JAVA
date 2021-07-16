aab1517bc6950f544fbfea1fa7f492f252fee97b
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-07-16 12:14:46 +0900

    Update Test_1546_평균.java

--- Test_1546_평균.java
+++ Test_1546_평균.java
@@ -2,21 +2,19 @@ package code;
 
 import java.util.Scanner;
 
-public class Code {
+public class T_평균 {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-//		int N = sc.nextInt();
-//		double arr[] = new double[N];
-		double arr[] = new double[sc.nextInt()];
 
-		double max = arr[0];
+		double arr[] = new double[sc.nextInt()];
+		double max = 0;
 		double sum = 0;
 
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
+			max = arr[0];
 
 			if (arr[i] > max) {
 				max = arr[i];
@@ -24,10 +22,9 @@ public class Code {
 		}
 
 		for (int i = 0; i < arr.length; i++) {
-			sum += arr[i] / max * 100;
+			sum += (arr[i] / max) * 100;
 		}
 
 		System.out.println(sum / arr.length);
-
 	}
 }
